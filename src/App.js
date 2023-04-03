f434a8e037c24204836ed91f27c89ffab4fe735b
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-04-03 19:16:59 +0200

    NavBar OK

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+import Routing from './routing/Routing';
+import NavBar from './components/NavBar/NavBar.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+
+    <Routing />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './NavBar.module.css'
+import { Link } from 'react-router-dom'
+ 
+const NavBar = () => {
+  return (
+   <nav className={styles.navContainer}>
+        <img src="/assets/logo.png" alt="logo"  className={styles.logo} />
+        <ul className={styles.navList}>
+            <li className={styles.navItem}>
+                <Link className={styles.navLink} to="/">Accueil</Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link className={styles.navLink} to="/">Profil</Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link className={styles.navLink} to="/">Réglage</Link>
+            </li>
+            <li className={styles.navItem}>
+                <Link className={styles.navLink} to="/">Communauté</Link>
+            </li>
+        </ul>
+
+
+   </nav>
+  )
+}
+
+export default NavBar
