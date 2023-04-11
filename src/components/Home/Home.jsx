142f43c590eb2787c7ab9ad1328095ae07cd14a1
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-04-11 02:57:12 +0200

    maj home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import { store } from '../../config'
-import { whereAreWe } from '../../config';
-
 
 const Home = () => {
-  
-  console.log("ligne 1 " , whereAreWe);
+
   console.log("ligne 1 " , store.getUserId(12));
 
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import ApiStore from "./Services/apiStore";
 import MockStore from "./Services/mockStore";
 
+// si la variable d'environnement REACT_APP_DATA_SOURCE est égale à "mock" on utilise le mockStore
+// sinon on utilise le apiStore
 export let store = process.env.REACT_APP_DATA_SOURCE === "mock" ? MockStore : ApiStore;
-export let whereAreWe = process.env.REACT_APP_DATA_SOURCE === "mock" ? "mock" : "api";
