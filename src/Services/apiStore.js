987187fa15458e411200dcdd84a232e06fe0e1f0
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-04-11 17:25:24 +0200

    store url fixed

--- src/Services/apiStore.js
+++ src/Services/apiStore.js
@@ -4,27 +4,32 @@ import AverageModel from "./Models/averageModel";
 import ActivityModel from "./Models/activityModel";
 import PerformanceModel from "./Models/perfsModel";
 
+const baseUrl = "http://localhost:3000/user/";
+
 const ApiStore = {
   getUserId: async (id) => {
-    const { data } = await axios.get(`http://localhost:3000/user/${id}`);
-    const userData = new UserDataModel(data.data)
-    console.log(userData)
-    return userData
+    const { data } = await axios.get(baseUrl + id);
+    const userData = new UserDataModel(data.data);
+    console.log(userData);
+    return userData;
   },
   getUserActivity: async (id) => {
-    const { data } = await axios.get(`http://localhost:3000/user/${id}/activity`);
-    const activityData = new ActivityModel(data.data)
-    return activityData.mappedData
+    const { data } = await axios.get(baseUrl + id + "/activity");
+    const activityData = new ActivityModel(data.data);
+    console.log(activityData);
+    return activityData.mappedData;
   },
   getUserAverageSession: async (id) => {
-    const { data } = await axios.get(`http://localhost:3000/user/${id}/average-sessions`);
-    const averageData = new AverageModel(data.data)
-    return averageData.mappedData
+    const { data } = await axios.get(baseUrl + id + "/average-sessions");
+    const averageData = new AverageModel(data.data);
+    console.log(averageData);
+    return averageData.mappedData;
   },
   getUserPerformance: async (id) => {
-    const { data } = await axios.get(`http://localhost:3000/user/${id}/performance`);
-    const performanceData = new PerformanceModel(data.data)
-    return performanceData.mappedData
+    const { data } = await axios.get(baseUrl + id + "/performance");
+    const performanceData = new PerformanceModel(data.data);
+    console.log(performanceData);
+    return performanceData.mappedData;
   },
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,10 @@ import { store } from '../../config'
 const Home = () => {
 
   console.log("ligne 1 " , store.getUserId(12));
+  console.log("ligne 2 " , store.getUserActivity(12));
+  console.log("ligne 3 " , store.getUserAverageSession(12));
+  console.log("ligne 4 " , store.getUserPerformance(12));
+
 
 
   return (
